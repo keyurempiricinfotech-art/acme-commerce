2227498622f6959f70d3b25492e95c7d29045928
keyurempiricinfotech-art <keyur.empiricinfotech@expert.micro1.ai>
2026-07-06 16:32:08 +0530

    Remove DB performance seed src/services/CheckoutService.ts

--- src/services/CheckoutService.ts
+++
@@ -1,9 +0,0 @@
-import { PaymentRepository } from "../payments/PaymentRepository";
-
-export class CheckoutService {
-  constructor(private readonly paymentRepository: PaymentRepository) {}
-
-  async paymentMethods(userId: number) {
-    return this.paymentRepository.findPaymentMethods(userId);
-  }
-}
